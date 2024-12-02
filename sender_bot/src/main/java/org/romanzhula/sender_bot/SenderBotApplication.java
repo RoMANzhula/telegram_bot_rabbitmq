c7026e2390f0fb5f5777d73e766f92ff5e26b7f8
RoMANzhula <roman1985al@ukr.net>
2024-12-02 20:30:36 +0200

    Add sender_bot modul

---
+++ sender_bot/src/main/java/org/romanzhula/sender_bot/SenderBotApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.sender_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenderBotApplication.class, args);
+    }
+
+}
