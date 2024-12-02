3f5ba7db60a288351cf5458eeecb350d0194c9a1
RoMANzhula <roman1985al@ukr.net>
2024-12-02 20:58:05 +0200

    Up GeneralBot - return answer method

--- sender_bot/src/main/java/org/romanzhula/sender_bot/telegram/GeneralBot.java
+++ sender_bot/src/main/java/org/romanzhula/sender_bot/telegram/GeneralBot.java
@@ -3,7 +3,9 @@ package org.romanzhula.sender_bot.telegram;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Log4j
 @Component
@@ -22,8 +24,25 @@ public class GeneralBot extends TelegramLongPollingBot {
             var message = update.getMessage();
             var chatId = message.getChatId();
             log.debug("chatId_" + chatId + ": " + message.getText());
+
+            var response = new SendMessage();
+            response.setChatId(chatId.toString());
+            response.setText("It's message from GeneralBot.class. Testing... answer to your message.");
+
+            sendAnswerMessage(response);
+
         } else {
             log.debug("Received update: " + update);
+        }
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException exception) {
+                log.error(exception);
+            }
         }
     }
 
