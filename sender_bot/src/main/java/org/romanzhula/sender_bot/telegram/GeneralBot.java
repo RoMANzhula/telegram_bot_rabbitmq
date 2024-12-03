0759c98d5ea00fe6802d2aa83fa598ba0ee0b1f4
RoMANzhula <roman1985al@ukr.net>
2024-12-03 22:11:13 +0200

    Up GeneralBot - sender-bot module

--- sender_bot/src/main/java/org/romanzhula/sender_bot/telegram/GeneralBot.java
+++ sender_bot/src/main/java/org/romanzhula/sender_bot/telegram/GeneralBot.java
@@ -1,6 +1,8 @@
 package org.romanzhula.sender_bot.telegram;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j;
+import org.romanzhula.sender_bot.controllers.MessageHandlerController;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -12,28 +14,42 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class GeneralBot extends TelegramLongPollingBot {
 
     private final BotSettings botSettings;
+    private final MessageHandlerController messageHandlerController;
 
-    public GeneralBot(BotSettings botSettings) {
+    public GeneralBot(
+            BotSettings botSettings,
+            MessageHandlerController messageHandlerController
+    ) {
         super(botSettings.getBotToken());
+
         this.botSettings = botSettings;
+        this.messageHandlerController = messageHandlerController;
+    }
+
+    @PostConstruct
+    public void init() {
+        messageHandlerController.registerBot(this);
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            var message = update.getMessage();
-            var chatId = message.getChatId();
-            log.debug("chatId_" + chatId + ": " + message.getText());
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//            var message = update.getMessage();
+//            var chatId = message.getChatId();
+//            log.debug("chatId_" + chatId + ": " + message.getText());
+//
+//            var response = new SendMessage();
+//            response.setChatId(chatId.toString());
+//            response.setText("It's message from GeneralBot.class. Testing... answer to your message.");
+//
+//            sendAnswerMessage(response);
+//
+//        } else {
+//            log.debug("Received update: " + update);
+//        }
 
-            var response = new SendMessage();
-            response.setChatId(chatId.toString());
-            response.setText("It's message from GeneralBot.class. Testing... answer to your message.");
+        messageHandlerController.processUpdate(update);
 
-            sendAnswerMessage(response);
-
-        } else {
-            log.debug("Received update: " + update);
-        }
     }
 
     public void sendAnswerMessage(SendMessage message) {
