afcf64e2495616d79cfa8218e8c68ef9f99e5d9b
RoMANzhula <roman1985al@ukr.net>
2024-12-03 22:14:16 +0200

    Add rabbitmq-buffer module

---
+++ rabbitmq_buffer/src/main/java/org/romanzhula/rabbitmq_buffer/models/RabbitmqQueue.java
@@ -0,0 +1,10 @@
+package org.romanzhula.rabbitmq_buffer.models;
+
+public class RabbitmqQueue {
+
+    public static final String TEXT_MESSAGE_UPDATE = "text_message_update";
+    public static final String DOCUMENT_MESSAGE_UPDATE = "document_message_update";
+    public static final String PHOTO_MESSAGE_UPDATE = "photo_message_update";
+    public static final String ANSWER_MESSAGE = "answer_message";
+
+}
