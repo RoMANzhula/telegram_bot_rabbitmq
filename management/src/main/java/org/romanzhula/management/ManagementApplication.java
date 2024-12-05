d3060d2de3bf9f0a1d2e40844726013c93657353
RoMANzhula <roman1985al@ukr.net>
2024-12-05 21:35:46 +0200

    Up ManagementApplication - management module - annotations for all modules

--- management/src/main/java/org/romanzhula/management/ManagementApplication.java
+++ management/src/main/java/org/romanzhula/management/ManagementApplication.java
@@ -2,8 +2,14 @@ package org.romanzhula.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("org.romanzhula.*")
+@EnableJpaRepositories("org.romanzhula.*")
+@EntityScan("org.romanzhula.*")
 public class ManagementApplication {
 
     public static void main(String[] args) {
