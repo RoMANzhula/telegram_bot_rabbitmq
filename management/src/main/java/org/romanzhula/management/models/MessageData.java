a27a288e1ab22d15544e36b5adbba624e16ef865
RoMANzhula <roman1985al@ukr.net>
2024-12-05 16:15:34 +0200

    Add MessageData - management module

---
+++ management/src/main/java/org/romanzhula/management/models/MessageData.java
@@ -0,0 +1,47 @@
+package org.romanzhula.management.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.type.SqlTypes;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(exclude = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "message_data")
+public class MessageData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Update event;
+
+//    @Override
+//    public final boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null) return false;
+//        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+//        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+//        if (thisEffectiveClass != oEffectiveClass) return false;
+//        MessageData that = (MessageData) o;
+//        return getId() != null && Objects.equals(getId(), that.getId());
+//    }
+//
+//    @Override
+//    public final int hashCode() {
+//        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+//    }
+
+}
