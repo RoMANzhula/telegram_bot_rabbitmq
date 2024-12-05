f511248f134024b872b1b2b53429698e636c95b9
RoMANzhula <roman1985al@ukr.net>
2024-12-05 16:28:25 +0200

    Up ConsumerServiceImpl - management module

--- management/src/main/java/org/romanzhula/management/services/implementations/ConsumerServiceImpl.java
+++ management/src/main/java/org/romanzhula/management/services/implementations/ConsumerServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.romanzhula.management.services.ConsumerService;
+import org.romanzhula.management.services.MessageHandleService;
 import org.romanzhula.management.services.ProducerService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -18,24 +19,19 @@ public class ConsumerServiceImpl implements ConsumerService {
 
 //    private static final Logger log = LogManager.getLogger(ConsumerServiceImpl.class);
 
-    private final ProducerService producerService;
+    private final MessageHandleService messageHandleService;
 
-    public ConsumerServiceImpl(ProducerService producerService) {
-        this.producerService = producerService;
+    public ConsumerServiceImpl(MessageHandleService messageHandleService) {
+        this.messageHandleService = messageHandleService;
     }
+
 
     @Override
     @RabbitListener(queues = "${rabbitmq.queue.name.text}")
     public void consumeTextMessageUpdate(Update update) {
         log.debug("Manager: Text msg is received successfully.");
 
-        Message message = update.getMessage();
-        SendMessage sendMessage = new SendMessage();
-
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from MANAGEMENT!");
-
-        producerService.produceAnswer(sendMessage);
+        messageHandleService.processTextMessage(update);
     }
 
     @Override
