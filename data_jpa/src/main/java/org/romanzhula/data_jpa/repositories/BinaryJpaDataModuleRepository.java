5150151da3d017fef024e807977b634f9013897d
RoMANzhula <roman1985al@ukr.net>
2024-12-06 20:15:10 +0200

    Add BinaryJpaDataModuleRepository - data_jpa module

---
+++ data_jpa/src/main/java/org/romanzhula/data_jpa/repositories/BinaryJpaDataModuleRepository.java
@@ -0,0 +1,11 @@
+package org.romanzhula.data_jpa.repositories;
+
+import org.romanzhula.data_jpa.models.BinaryJpaDataModule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface BinaryJpaDataModuleRepository extends JpaRepository<BinaryJpaDataModule, Long> {
+
+}
