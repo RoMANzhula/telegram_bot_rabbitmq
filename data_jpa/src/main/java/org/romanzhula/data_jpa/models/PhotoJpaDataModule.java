a581ad52b15d3d1568eb93effee34b40621c4028
RoMANzhula <roman1985al@ukr.net>
2024-12-06 20:11:29 +0200

    Add PhotoJpaDataModule - data_jpa module

---
+++ data_jpa/src/main/java/org/romanzhula/data_jpa/models/PhotoJpaDataModule.java
@@ -0,0 +1,47 @@
+package org.romanzhula.data_jpa.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "photos_app")
+@Entity
+public class PhotoJpaDataModule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String telegramId;
+
+    @OneToOne
+    private BinaryJpaDataModule binaryData;
+
+    private Integer fileLength;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhotoJpaDataModule that = (PhotoJpaDataModule) o;
+
+        return Objects.equals(telegramId, that.telegramId) &&
+                Objects.equals(binaryData, that.binaryData) &&
+                Objects.equals(fileLength, that.fileLength)
+        ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(telegramId, binaryData, fileLength);
+    }
+
+}
