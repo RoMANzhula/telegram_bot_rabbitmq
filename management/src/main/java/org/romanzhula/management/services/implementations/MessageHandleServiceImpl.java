b7e692e61bfc43e3026a8c29c46e8ba0687b99dd
RoMANzhula <roman1985al@ukr.net>
2024-12-06 19:23:02 +0200

    Up MessageHandleServiceImpl - management module

--- management/src/main/java/org/romanzhula/management/services/implementations/MessageHandleServiceImpl.java
+++ management/src/main/java/org/romanzhula/management/services/implementations/MessageHandleServiceImpl.java
@@ -1,19 +1,24 @@
 package org.romanzhula.management.services.implementations;
 
+import lombok.extern.slf4j.Slf4j;
 import org.romanzhula.data_jpa.models.UserJpaDataModule;
 import org.romanzhula.data_jpa.models.enums.UserState;
 import org.romanzhula.data_jpa.repositories.UserJpaDataModuleRepository;
+import org.romanzhula.management.enums.TelegramCommands;
 import org.romanzhula.management.models.MessageData;
 import org.romanzhula.management.repositories.MessageDataRepository;
 import org.romanzhula.management.services.MessageHandleService;
 import org.romanzhula.management.services.ProducerService;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
+import static org.romanzhula.data_jpa.models.enums.UserState.COMMON_STATE;
+import static org.romanzhula.management.enums.TelegramCommands.*;
 
+
+@Slf4j
 @Service
 public class MessageHandleServiceImpl implements MessageHandleService {
 
@@ -35,15 +40,130 @@ public class MessageHandleServiceImpl implements MessageHandleService {
     public void processTextMessage(Update update) {
         saveMessageData(update);
 
-        Message textMessage = update.getMessage();
-        User telegramUser = textMessage.getFrom();
-        UserJpaDataModule userFromJpaData = findOrSaveUser(telegramUser);
+        UserJpaDataModule userFromJpaData = findOrSaveUser(update);
+        UserState userState = userFromJpaData.getUserState();
+        String text = update.getMessage().getText();
+        String output = "";
 
-        Message message = update.getMessage();
+        if (text == null || userState == null) {
+            log.error("Input data cannot be null!");
+            throw new IllegalArgumentException("Input data cannot be null!");
+        }
+
+        if (CANCEL.equals(text)) {
+            output = processStop(userFromJpaData);
+        } else {
+            switch (userState) {
+                case COMMON_STATE -> {
+                    output = processTelegramCommand(userFromJpaData, text);
+                }
+                case WAIT_EMAIL_STATE -> {
+                    log.info("You need to add your email!"); // TODO: change to method for MailSender
+                }
+                default -> {
+                    log.error("Unknown command: {}", userState);
+                    output = "Unknown command! Please input '/cancel' and try again. Use '/help'.";
+                }
+            }
+        }
+
+        Long chatId = update.getMessage().getChatId();
+
+        sendAnswer(output, chatId);
+
+    }
+
+    @Override
+    public void processPhotoMessage(Update update) {
+        saveMessageData(update);
+
+        UserJpaDataModule userFromJpaData = findOrSaveUser(update);
+        Long chatId = update.getMessage().getChatId();
+
+        if (isNotAllowToBeSent(chatId, userFromJpaData)) {
+            log.error("Does not allow content to be sent.");
+            return;
+        }
+
+        // TODO: add photo loading method
+        String answer = "Photo has been successfully uploaded. Download link: ...";
+        sendAnswer(answer, chatId);
+    }
+
+    @Override
+    public void processDocumentMessage(Update update) {
+        saveMessageData(update);
+
+        UserJpaDataModule userFromJpaData = findOrSaveUser(update);
+        Long chatId = update.getMessage().getChatId();
+
+        if (isNotAllowToBeSent(chatId, userFromJpaData)) {
+            log.error("Does not allow content to be sent.");
+            return;
+        }
+
+        // TODO: add document loading method
+        String answer = "Document has been successfully uploaded. Download link: ...";
+        sendAnswer(answer, chatId);
+    }
+
+    private boolean isNotAllowToBeSent(Long chatId, UserJpaDataModule userFromJpaData) {
+        UserState userState = userFromJpaData.getUserState();
+
+        if (!userFromJpaData.getIsActive()) {
+            String errorMessage = "Register or activate your account to download content.";
+
+            sendAnswer(errorMessage, chatId);
+
+            return true;
+        } else if (!COMMON_STATE.equals(userState)) {
+            String errorMessage = "Cancel the current command with '/cancel' to send the files.";
+
+            sendAnswer(errorMessage, chatId);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private String processTelegramCommand(UserJpaDataModule userFromJpaData, String command) {
+
+        if (REGISTRATION.equals(command)) {
+            log.info("Sorry. Currently not work!");
+            return "Sorry. Currently not work!";
+        } else if (HELP.equals(command)) {
+            log.info("User entered the command '/help'");
+            return commandHelp();
+        } else if (START.equals(command)) {
+            log.info("User entered the command '/start'");
+            return "Hello in our Bot. Please input '/help' to look for all common commands.";
+        } else {
+            log.warn("Received unknown command: {}", command);
+            return "Unknown command! Please input '/cancel' and try again. Or use '/help'.";
+        }
+
+    }
+
+    private String commandHelp() {
+        return "List of all commands:\n" +
+                "/start - greeting message;\n" +
+                "/cancel - cancel the current command;\n" +
+                "/registration - register new user"
+        ;
+    }
+
+    private String processStop(UserJpaDataModule userFromJpaData) {
+        userFromJpaData.setUserState(COMMON_STATE);
+        userJpaDataModuleRepository.save(userFromJpaData);
+
+        return "Success! The command is canceled.";
+    }
+
+    private void sendAnswer(String output, Long chatId) {
         SendMessage sendMessage = new SendMessage();
-
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from MANAGEMENT!");
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(output);
 
         producerService.produceAnswer(sendMessage);
     }
@@ -57,8 +177,11 @@ public class MessageHandleServiceImpl implements MessageHandleService {
         messageDataRepository.save(messageData);
     }
 
-    private UserJpaDataModule findOrSaveUser(User telegramUser) {
-        UserJpaDataModule persistentUserFromDataJpaModule = userJpaDataModuleRepository.findByTelegramId(telegramUser.getId());
+    private UserJpaDataModule findOrSaveUser(Update update) {
+        User telegramUser = update.getMessage().getFrom();
+
+        UserJpaDataModule persistentUserFromDataJpaModule =
+                userJpaDataModuleRepository.findByTelegramId(telegramUser.getId());
 
         if (persistentUserFromDataJpaModule == null) {
             UserJpaDataModule transientUser = UserJpaDataModule.builder()
@@ -66,9 +189,9 @@ public class MessageHandleServiceImpl implements MessageHandleService {
                     .firstName(telegramUser.getFirstName())
                     .lastName(telegramUser.getLastName())
                     .userName(telegramUser.getUserName())
-                    // TODO change to false after added MailSender
+                    // TODO: change to false after added MailSender
                     .isActive(true)
-                    .userState(UserState.COMMON_STATE)
+                    .userState(COMMON_STATE)
                     .build()
             ;
 
