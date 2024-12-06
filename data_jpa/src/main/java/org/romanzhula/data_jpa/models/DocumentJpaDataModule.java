62296e904a60eed478ee7dcfbf131bc6e363c92d
RoMANzhula <roman1985al@ukr.net>
2024-12-06 20:12:04 +0200

    Add DocumentJpaDataModule - data_jpa module

---
+++ data_jpa/src/main/java/org/romanzhula/data_jpa/models/DocumentJpaDataModule.java
@@ -0,0 +1,52 @@
+package org.romanzhula.data_jpa.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "documents_app")
+@Entity
+public class DocumentJpaDataModule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String telegramId;
+
+    private String name;
+
+    @OneToOne
+    private BinaryJpaDataModule binaryData;
+
+    private String mimeType;
+
+    private Long fileLength;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DocumentJpaDataModule that = (DocumentJpaDataModule) o;
+
+        return Objects.equals(telegramId, that.telegramId) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(binaryData, that.binaryData) &&
+                Objects.equals(mimeType, that.mimeType) &&
+                Objects.equals(fileLength, that.fileLength)
+        ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(telegramId, name, binaryData, mimeType, fileLength);
+    }
+}
