048fedd8ca823c4d82a6d787782c4932493ca98c
RoMANzhula <roman1985al@ukr.net>
2024-12-07 23:23:17 +0200

    Up FileService - management module

--- management/src/main/java/org/romanzhula/management/services/FileService.java
+++ management/src/main/java/org/romanzhula/management/services/FileService.java
@@ -1,10 +1,16 @@
 package org.romanzhula.management.services;
 
 import org.romanzhula.data_jpa.models.DocumentJpaDataModule;
+import org.romanzhula.data_jpa.models.PhotoJpaDataModule;
+import org.romanzhula.management.enums.DownloadLinkType;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 public interface FileService {
 
     DocumentJpaDataModule processDocument(Message inputFromTelegramMessage);
+
+    PhotoJpaDataModule processPhoto(Message inputFromTelegramMessage);
+
+    String generateLink(Long fileId, DownloadLinkType downloadLinkType);
 
 }
