5a574ef6202277dd910f272a3b8eab03fcbfd304
RoMANzhula <roman1985al@ukr.net>
2024-12-09 00:57:52 +0200

    Up FileController - rest_dispatcher module

--- rest_dispatcher/src/main/java/org/romanzhula/rest_dispatcher/controllers/FileController.java
+++ rest_dispatcher/src/main/java/org/romanzhula/rest_dispatcher/controllers/FileController.java
@@ -1,20 +1,13 @@
 package org.romanzhula.rest_dispatcher.controllers;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
-import org.romanzhula.data_jpa.models.BinaryJpaDataModule;
-import org.romanzhula.data_jpa.models.DocumentJpaDataModule;
-import org.romanzhula.data_jpa.models.PhotoJpaDataModule;
 import org.romanzhula.rest_dispatcher.services.FileService;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Log4j
+
 @RequiredArgsConstructor
 @RequestMapping("/api/file")
-@RestControllerAdvice
 @RestController
 public class FileController {
 
