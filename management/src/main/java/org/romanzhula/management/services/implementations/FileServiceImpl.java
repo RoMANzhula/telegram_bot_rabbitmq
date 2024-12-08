c0f2d4e3f15c3d8fa18c665ebce33ea6db46c3ac
RoMANzhula <roman1985al@ukr.net>
2024-12-08 16:32:19 +0200

    Up FileServiceImpl - management module - saving data to db

--- management/src/main/java/org/romanzhula/management/services/implementations/FileServiceImpl.java
+++ management/src/main/java/org/romanzhula/management/services/implementations/FileServiceImpl.java
@@ -1,11 +1,17 @@
 package org.romanzhula.management.services.implementations;
 
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.LogManager;
+import org.hashids.Hashids;
 import org.json.JSONObject;
 import lombok.extern.log4j.Log4j;
 import org.romanzhula.data_jpa.models.BinaryJpaDataModule;
 import org.romanzhula.data_jpa.models.DocumentJpaDataModule;
+import org.romanzhula.data_jpa.models.PhotoJpaDataModule;
 import org.romanzhula.data_jpa.repositories.BinaryJpaDataModuleRepository;
 import org.romanzhula.data_jpa.repositories.DocumentJpaDataModuleRepository;
+import org.romanzhula.data_jpa.repositories.PhotoJpaDataModuleRepository;
+import org.romanzhula.management.enums.DownloadLinkType;
 import org.romanzhula.management.services.FileService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -15,12 +21,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.objects.Document;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 
-
+@RequiredArgsConstructor
 @Log4j
 @Service
 public class FileServiceImpl implements FileService {
@@ -34,18 +41,14 @@ public class FileServiceImpl implements FileService {
     @Value("${telegram.bot.service.file_info.uri}")
     private String telegramFileInfoUri;
 
+    @Value("${telegram.download_link.address}")
+    private String downloadLinkAddress;
+
 
     private final DocumentJpaDataModuleRepository documentJpaDataModuleRepository;
     private final BinaryJpaDataModuleRepository binaryJpaDataModuleRepository;
-
-
-    public FileServiceImpl(
-            DocumentJpaDataModuleRepository documentJpaDataModuleRepository,
-            BinaryJpaDataModuleRepository binaryJpaDataModuleRepository
-    ) {
-        this.documentJpaDataModuleRepository = documentJpaDataModuleRepository;
-        this.binaryJpaDataModuleRepository = binaryJpaDataModuleRepository;
-    }
+    private final PhotoJpaDataModuleRepository photoJpaDataModuleRepository;
+    private final Hashids hashids;
 
 
     @Override
@@ -56,13 +59,54 @@ public class FileServiceImpl implements FileService {
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             BinaryJpaDataModule persistentBinaryData = getPersistentBinaryData(responseEntity);
-            var transientDocumentJpaDataModule = buildTransientDocumentJpaDataModule(document, persistentBinaryData);
+            DocumentJpaDataModule transientDocumentJpaDataModule =
+                    buildTransientDocumentJpaDataModule(document, persistentBinaryData);
 
             return documentJpaDataModuleRepository.save(transientDocumentJpaDataModule);
         } else {
             log.error("Bad response from file service: " + responseEntity);
             throw new RuntimeException("Bad response from file service: " + responseEntity);
         }
+    }
+
+    @Override
+    public PhotoJpaDataModule processPhoto(Message inputFromTelegramMessage) {
+        int photoSize = inputFromTelegramMessage.getPhoto().size();
+        int indexOfPhoto = photoSize > 1 ? inputFromTelegramMessage.getPhoto().size() - 1 : 0;
+        PhotoSize photoFromBot = inputFromTelegramMessage.getPhoto().get(indexOfPhoto);
+        String fileId = photoFromBot.getFileId();
+
+        ResponseEntity<String> responseEntity = getFilePath(fileId);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            BinaryJpaDataModule persistentBinaryData = getPersistentBinaryData(responseEntity);
+            PhotoJpaDataModule transientPhotoJpaDataModule =
+                    buildTransientPhotoJpaDataModule(photoFromBot, persistentBinaryData);
+
+            return photoJpaDataModuleRepository.save(transientPhotoJpaDataModule);
+        } else {
+            log.error("Bad response from file service: " + responseEntity);
+            throw new RuntimeException("Bad response from file service: " + responseEntity);
+        }
+    }
+
+    @Override
+    public String generateLink(Long fileId, DownloadLinkType downloadLinkType) {
+        String hashId = hashids.encode(fileId);
+
+        return downloadLinkAddress + "/api" + downloadLinkType + "?id=" + hashId;
+    }
+
+    private PhotoJpaDataModule buildTransientPhotoJpaDataModule(
+            PhotoSize photoFromBot,
+            BinaryJpaDataModule persistentBinaryData
+    ) {
+        return PhotoJpaDataModule.builder()
+                .telegramId(photoFromBot.getFileId())
+                .binaryData(persistentBinaryData)
+                .fileLength(photoFromBot.getFileSize())
+                .build()
+        ;
     }
 
     private DocumentJpaDataModule buildTransientDocumentJpaDataModule(
