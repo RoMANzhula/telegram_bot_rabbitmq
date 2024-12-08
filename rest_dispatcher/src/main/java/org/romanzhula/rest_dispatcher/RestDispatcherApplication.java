6d487d37b0ed87124cb00ad3f05f887f2d6a6600
RoMANzhula <roman1985al@ukr.net>
2024-12-08 18:03:37 +0200

    Up RestDispatcherApplication - rest_dispatcher module - scanning all components

--- rest_dispatcher/src/main/java/org/romanzhula/rest_dispatcher/RestDispatcherApplication.java
+++ rest_dispatcher/src/main/java/org/romanzhula/rest_dispatcher/RestDispatcherApplication.java
@@ -2,7 +2,13 @@ package org.romanzhula.rest_dispatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan("org.romanzhula.*")
+@EnableJpaRepositories("org.romanzhula.*")
+@EntityScan("org.romanzhula.*")
 @SpringBootApplication
 public class RestDispatcherApplication {
 
