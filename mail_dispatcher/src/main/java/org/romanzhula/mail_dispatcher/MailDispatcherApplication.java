315eab869eac4bb9ac4acc40e06097b2d01ed919
RoMANzhula <roman1985al@ukr.net>
2024-12-09 23:22:44 +0200

    Add mail_dispatcher module

---
+++ mail_dispatcher/src/main/java/org/romanzhula/mail_dispatcher/MailDispatcherApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.mail_dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailDispatcherApplication.class, args);
+    }
+
+}
