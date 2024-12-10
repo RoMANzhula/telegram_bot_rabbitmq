59819568b941e90387a39858801042a547c8358e
RoMANzhula <roman1985al@ukr.net>
2024-12-10 23:34:26 +0200

    Add ConsumerServiceImpl - mail_dispatcher module

---
+++ mail_dispatcher/src/main/java/org/romanzhula/mail_dispatcher/services/implementations/ConsumerServiceImpl.java
@@ -0,0 +1,25 @@
+package org.romanzhula.mail_dispatcher.services.implementations;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.romanzhula.data_jpa.objects.MailDataJpaDataModule;
+import org.romanzhula.mail_dispatcher.services.ConsumerService;
+import org.romanzhula.mail_dispatcher.services.MailSenderService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+
+@Log4j
+@RequiredArgsConstructor
+@Service
+public class ConsumerServiceImpl implements ConsumerService {
+
+    private final MailSenderService mailSenderService;
+
+    @Override
+    @RabbitListener(queues = "${rabbitmq.queue.name.registration.mail}")
+    public void consumeMailRegister(MailDataJpaDataModule mailData) {
+        mailSenderService.send(mailData);
+    }
+
+}
