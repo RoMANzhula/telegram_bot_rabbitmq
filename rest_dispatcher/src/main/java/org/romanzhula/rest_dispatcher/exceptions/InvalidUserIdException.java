224dec46018d2791efc0c140992f3755614d466a
RoMANzhula <roman1985al@ukr.net>
2024-12-11 19:45:17 +0200

    Add InvalidUserIdException - rest_dispatcher module

---
+++ rest_dispatcher/src/main/java/org/romanzhula/rest_dispatcher/exceptions/InvalidUserIdException.java
@@ -0,0 +1,9 @@
+package org.romanzhula.rest_dispatcher.exceptions;
+
+public class InvalidUserIdException extends RuntimeException {
+
+    public InvalidUserIdException(String message) {
+        super(message);
+    }
+
+}
