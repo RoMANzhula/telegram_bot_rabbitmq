c6f078e4146c56cd1b287594238c87d0fc6e0e5b
RoMANzhula <roman1985al@ukr.net>
2024-12-11 19:19:59 +0200

    Up UserJpaDataModuleRepository - data_jpa module

--- data_jpa/src/main/java/org/romanzhula/data_jpa/repositories/UserJpaDataModuleRepository.java
+++ data_jpa/src/main/java/org/romanzhula/data_jpa/repositories/UserJpaDataModuleRepository.java
@@ -4,9 +4,15 @@ import org.romanzhula.data_jpa.models.UserJpaDataModule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository("userJpaDataModuleRepository")
 public interface UserJpaDataModuleRepository extends JpaRepository<UserJpaDataModule, Long> {
 
-    UserJpaDataModule findByTelegramId(Long id);
+    Optional<UserJpaDataModule> findByTelegramId(Long id);
+
+    Optional<UserJpaDataModule> findById(Long id);
+
+    Optional<UserJpaDataModule> findByEmail(String email);
 
 }
